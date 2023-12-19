937711300c04848304bfd3204470e6a6085b7a60
Carol-Rocha <caroldsr1@gmail.com>
2023-12-19 14:02:46 -0300

    fix: fix card and home components

--- src/app/shared/card-busca/card-busca.component.ts
+++ src/app/shared/card-busca/card-busca.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PromocaoService } from 'src/app/core/services/promocao.service';
+import { IPromocao } from 'src/app/core/types/types';
 
 @Component({
   selector: 'app-card-busca',
   templateUrl: './card-busca.component.html',
-  styleUrls: ['./card-busca.component.scss']
+  styleUrls: ['./card-busca.component.scss'],
 })
 export class CardBuscaComponent implements OnInit {
+  @Input() promocao!: IPromocao;
 
-  constructor() { }
+  constructor(private promocoesService: PromocaoService) {}
 
   ngOnInit(): void {
+    // this.promocoesService.listarPromocoes().subscribe((item) => {
+    //   this.promocoes = item;
+    // });
   }
-
 }
